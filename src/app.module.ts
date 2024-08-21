424081b77423c77803eb092e56b44ae604565904
Jisu <hellojs242@gmail.com>
2024-08-21 20:53:16 +0900

    :hammer: chore: TypeORM으로 MySQL 연결 설정 #2
    - Snake naming strategy 적용
    - env file을 이용한 TypeORM module 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'mysql',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT || '3306'),
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        autoLoadEntities: true,
+        synchronize: true,
+        logging: true,
+        namingStrategy: new SnakeNamingStrategy(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
