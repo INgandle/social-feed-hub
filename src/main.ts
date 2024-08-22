f8238399183c0b465799d2c86bb6952c5724d0cc
Jisu <hellojs242@gmail.com>
2024-08-22 22:03:57 +0900

    :hammer: chore: swagger 설정 추가 #6
    - @nestjs/swagger 로 스웨거 문서 설정
    - nestjs-cli plugin 사용하여 주석, dto 감지 가능합니다

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+const config = new DocumentBuilder()
+  .setTitle('Social feed hub API')
+  .setDescription('소셜 미디어 통합 feed 서비스 (가제) 의 API 문서입니다.')
+  .setVersion('1.0')
+  .addTag('SocialFeedHub')
+  .build();
 
 const bootstrap = async (): Promise<void> => {
   const app = await NestFactory.create(AppModule);
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 };
 bootstrap();
