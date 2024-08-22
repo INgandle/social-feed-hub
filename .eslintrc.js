84693e671535cd0d9826494b9afedfdeccb293d3
Doyeon Kim <doyeonkim.yeon@gmail.com>
2024-08-22 22:07:06 +0900

    :hammer: chore: eslint에 설정된 코드 포맷팅 규칙을 prettier 규칙으로 변경 (#8)

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,11 +30,6 @@ module.exports = {
     'prettier/prettier': ['error', { endOfLine: 'auto' }],
     '@typescript-eslint/explicit-function-return-type': ['warn', { allowExpressions: true }], // 함수의 반환 타입 명시
     '@typescript-eslint/no-explicit-any': 'error', // any 사용시 경고
-    'max-len': ['error', { code: 120, tabWidth: 2 }],
-    indent: 0,
-    '@typescript-eslint/indent': ['error', 2],
     'no-else-return': 'warn', // else 뒤에 바로 return 경고
-    'brace-style': [1, '1tbs', { allowSingleLine: false }], // 중괄호 스타일을 1tbs로, 단일 라인 중괄호는 x
-    'no-confusing-arrow': ['warn', { allowParens: true }], // 혼동을 주는 화살표 함수 경고(괄호를 포함하면 허용)
   },
 };
