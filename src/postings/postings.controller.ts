66d1c0d5fa1c54082d4bfa1a3e547f349102287b
ooheunda <ooheunda@gmail.com>
2024-08-25 23:17:23 +0900

    :sparkles: feat: 게시물 상세 조회 API #13

--- src/postings/postings.controller.ts
+++ src/postings/postings.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { PostingsService } from './postings.service';
+import { PostingResponseDto } from './dto/posting-response.dto';
 
 @Controller('postings')
 export class PostingsController {
   constructor(private readonly postingsService: PostingsService) {}
+
+  @Get()
+  async findAll(): Promise<PostingResponseDto[]> {
+    return await this.postingsService.findAll();
+  }
+
+  // TODO: AuthGuard 적용
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<PostingResponseDto> {
+    return await this.postingsService.findOne(id);
+  }
 }
--- src/postings/postings.service.ts
+++ src/postings/postings.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import _ from 'lodash';
+
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Posting } from 'src/entities/posting.entity';
+import { PostingResponseDto } from './dto/posting-response.dto';
 
 @Injectable()
-export class PostingsService {}
+export class PostingsService {
+  constructor(@InjectRepository(Posting) private readonly postingRepository: Repository<Posting>) {}
+
+  async findAll(): Promise<PostingResponseDto[]> {}
+
+  /**
+   * 게시물을 상세 조회합니다. 조회수가 1씩 증가합니다.
+   * @param id 게시물 PK(uuid)
+   * @returns id에 해당하는 게시물
+   */
+  async findOne(id: string): Promise<PostingResponseDto> {
+    const posting = await this.postingRepository.findOneBy({ id });
+
+    if (_.isNil(posting)) {
+      throw new NotFoundException('posting not found');
+    }
+
+    this.postingRepository.update(id, { likeCount: posting.likeCount + 1 });
+
+    return posting;
+  }
+}
