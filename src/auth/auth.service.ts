f3332eef7cdc71a77ac8ca9d64d0e508910888c6
Doyeon Kim <doyeonkim.yeon@gmail.com>
2024-08-26 16:54:15 +0900

    :recycle: refactor: payload의 식별자(sub)의 위치를 맨 앞으로 수정 (#11)

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -35,7 +35,7 @@ export class AuthService {
   }
 
   async login(user: UserResponseDto) {
-    const payload = { name: user.name, sub: user.id, email: user.email, accountName: user.accountName };
+    const payload = { sub: user.id, name: user.name, email: user.email, accountName: user.accountName };
     return {
       access_token: this.jwtService.sign(payload),
     };
