0d24a648f5b354c30d474cc05c898a794ea62996
Doyeon Kim <doyeonkim.yeon@gmail.com>
2024-08-26 17:16:28 +0900

    :bug: fix: 사용자 로그인: request.user에 accountName 추가 (#11)
    - 통계 api에서 사용하기 위해 추가

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,6 +1,20 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
+import { User } from 'src/entities/user.entity';
+
+type JwtUser = {
+  userId: User['id'];
+  email: User['email'];
+  accountName: User['accountName'];
+};
+
+type Payload = {
+  sub: User['id'];
+  name: User['name'];
+  email: User['email'];
+  accountName: User['accountName'];
+};
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -12,7 +26,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: any) {
-    return { userId: payload.sub, email: payload.email };
+  async validate(payload: Payload): Promise<JwtUser> {
+    return { userId: payload.sub, email: payload.email, accountName: payload.accountName };
   }
 }
