986d5d94287d00d0bae3a1b9fec468bfec7fc911
ooheunda <ooheunda@gmail.com>
2024-08-28 03:08:20 +0900

    :sparkles: feat: AuthGuard 적용 및 any 타입 처리

--- src/postings/postings.controller.ts
+++ src/postings/postings.controller.ts
@@ -1,21 +1,24 @@
-import { Controller, Get, Param, Query, Request } from '@nestjs/common';
+import { Controller, Get, Param, Query, Req, UseGuards } from '@nestjs/common';
+import { Request } from 'express';
+
 import { PostingsService } from './postings.service';
 import { PostingResponseDto } from './dto/posting-response.dto';
 import { PostingQueryDto } from './dto/posting-query.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from 'src/entities/user.entity';
 
 @Controller('postings')
 export class PostingsController {
   constructor(private readonly postingsService: PostingsService) {}
 
-  // TODO: AuthGuard 적용
+  @UseGuards(AuthGuard('jwt'))
   @Get()
-  async findAll(@Request() req: any, @Query() queries: PostingQueryDto): Promise<PostingResponseDto[]> {
-    // TODO: any 해결
-    const hashtag = queries.hashtag || req.user.accountName;
+  async findAll(@Req() req: Request, @Query() queries: PostingQueryDto): Promise<PostingResponseDto[]> {
+    const hashtag = queries.hashtag || (req.user as User).accountName;
     return await this.postingsService.findAll(hashtag, queries);
   }
 
-  // TODO: AuthGuard 적용
+  @UseGuards(AuthGuard('jwt'))
   @Get(':id')
   async findOne(@Param('id') id: string): Promise<PostingResponseDto> {
     return await this.postingsService.findOne(id);
