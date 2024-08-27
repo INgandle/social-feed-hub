4d33ffe9ddea3967dd1f8c594779bece6a4ab4b2
ooheunda <ooheunda@gmail.com>
2024-08-28 03:15:32 +0900

    :recycle: refactor: 상세 조회 API 복잡한 쿼리빌더 코드 함수화

--- src/postings/postings.service.ts
+++ src/postings/postings.service.ts
@@ -16,10 +16,43 @@ export class PostingsService {
    * @returns 게시물 목록. 없는 경우 빈 배열
    */
   async findAll(hashtag: string, queries: PostingQueryDto): Promise<PostingResponseDto[]> {
-    const { type, orderBy, sortOrder, searchBy, search, pageCount, page } = queries;
-    if (isNaN(pageCount) || isNaN(page)) {
+    if (isNaN(queries.pageCount) || isNaN(queries.page)) {
       throw new BadRequestException();
     }
+
+    const rawPostings = await this.getRawPostings(hashtag, queries);
+
+    // hashtag depth 정리 & content 글자 수 제한 적용해 반환
+    return rawPostings.map((rawPosting) => {
+      const posting = this.getPostingObjWithHashtags(rawPosting);
+      posting.content = posting.content.substring(0, 20);
+
+      return posting;
+    });
+  }
+
+  /**
+   * 게시물을 상세 조회합니다. 조회수가 1씩 증가합니다.
+   * @param id 게시물 PK(uuid)
+   * @returns id에 해당하는 게시물
+   */
+  async findOne(id: string): Promise<PostingResponseDto> {
+    const posting = await this.postingRepository.findOne({
+      where: { id },
+      relations: ['postingHashtags', 'postingHashtags.hashtag'],
+    });
+
+    if (posting === null) {
+      throw new NotFoundException('posting not found');
+    }
+    this.postingRepository.update(id, { viewCount: posting.viewCount + 1 });
+
+    return this.getPostingObjWithHashtags(posting);
+  }
+
+  // 입력된 쿼리 파라미터를 바탕으로 DB 쿼리를 빌드해 게시물 목록을 가져옵니다.
+  private async getRawPostings(hashtag: string, queries: PostingQueryDto): Promise<Posting[]> {
+    const { type, orderBy, sortOrder, searchBy, search, pageCount, page } = queries;
 
     const queryBuilder = this.postingRepository.createQueryBuilder('posting');
 
@@ -44,38 +77,13 @@ export class PostingsService {
     }
 
     // 입력된 값대로 정렬해 페이지네이션
-    const postings = await queryBuilder
+    const rawPostings = await queryBuilder
       .orderBy(`posting.${orderBy}`, sortOrder)
       .skip(page * pageCount)
       .take(pageCount)
       .getMany();
 
-    // hashtag depth 정리 & content 글자 수 제한 적용해 반환
-    return postings.map((posting) => {
-      const arrangedPosting = this.getPostingObjWithHashtags(posting);
-      arrangedPosting.content = arrangedPosting.content.substring(0, 20);
-
-      return arrangedPosting;
-    });
-  }
-
-  /**
-   * 게시물을 상세 조회합니다. 조회수가 1씩 증가합니다.
-   * @param id 게시물 PK(uuid)
-   * @returns id에 해당하는 게시물
-   */
-  async findOne(id: string): Promise<PostingResponseDto> {
-    const posting = await this.postingRepository.findOne({
-      where: { id },
-      relations: ['postingHashtags', 'postingHashtags.hashtag'],
-    });
-
-    if (posting === null) {
-      throw new NotFoundException('posting not found');
-    }
-    this.postingRepository.update(id, { viewCount: posting.viewCount + 1 });
-
-    return this.getPostingObjWithHashtags(posting);
+    return rawPostings;
   }
 
   // 조인으로 인한 hashtag의 depth를 정리한 새 객체를 반환합니다.
